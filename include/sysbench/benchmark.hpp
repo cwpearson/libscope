d10546ec882ae44f5a7b16f0a8eecf3df7006e88
Carl Pearson <carl.w.pearson@gmail.com>
2020-06-16 14:01:04 -0500

    more benchmark loop utility macros

--- include/sysbench/benchmark.hpp
+++ include/sysbench/benchmark.hpp
@@ -1,14 +1,20 @@
 #pragma once
 
 // to be used in benchmark loop
-#define OR_SKIP_LOOP(stmt, msg)                                                                                        \
-  if (PRINT_IF_ERROR(stmt)) {                                                                                          \
-    state.SkipWithError(msg);                                                                                          \
-    break;                                                                                                             \
+#define OR_SKIP_AND_BREAK(stmt, msg)                                           \
+  if (PRINT_IF_ERROR(stmt)) {                                                  \
+    state.SkipWithError(msg);                                                  \
+    break;                                                                     \
   }
 
 // during setup or teardown
-#define OR_SKIP(stmt, msg)                                                                                             \
-  if (PRINT_IF_ERROR(stmt)) {                                                                                          \
-    state.SkipWithError(msg);                                                                                          \
+#define OR_SKIP(stmt, msg)                                                     \
+  if (PRINT_IF_ERROR(stmt)) {                                                  \
+    state.SkipWithError(msg);                                                  \
+  }
+
+#define OR_SKIP_AND_RETURN(stmt, msg)                                          \
+  if (PRINT_IF_ERROR(stmt)) {                                                  \
+    state.SkipWithError(msg);                                                  \
+    return;                                                                    \
   }
