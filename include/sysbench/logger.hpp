38aa2235c607bb1013e396887644f0a7be81fee6
Carl Pearson <carl.w.pearson@gmail.com>
2020-06-16 14:01:43 -0500

    control logger lifetime

--- include/sysbench/logger.hpp
+++ include/sysbench/logger.hpp
@@ -6,6 +6,12 @@ namespace sysbench {
   namespace logger {
     extern std::shared_ptr<spdlog::logger> console;
 } // namespace logger
+
+namespace logging {
+  /* set up loggers */
+  void init();
+}
+
 } // namespace sysbench
 
 #define LOG(level, ...) sysbench::logger::console->level(__VA_ARGS__)
