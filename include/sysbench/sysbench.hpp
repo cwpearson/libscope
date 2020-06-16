46ae879e440870f6f42c81714218e01dd82a0c87
Carl Pearson <carl.w.pearson@gmail.com>
2020-06-16 14:02:09 -0500

    add more headers

--- include/sysbench/sysbench.hpp
+++ include/sysbench/sysbench.hpp
@@ -2,10 +2,11 @@
 
 #include "benchmark/benchmark.h"
 
-#include "sysbench/init.hpp"
+#include "sysbench/benchmark.hpp"
+#include "sysbench/cache.hpp"
 #include "sysbench/cuda.hpp"
 #include "sysbench/defer.hpp"
+#include "sysbench/init.hpp"
 #include "sysbench/logger.hpp"
 #include "sysbench/numa.hpp"
 #include "sysbench/page_size.hpp"
-
