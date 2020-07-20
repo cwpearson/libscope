5a970ebddd15eb357bcc9c618cc789bb8a3e6057
Carl Pearson <carl.w.pearson@gmail.com>
2020-07-20 13:11:50 -0600

    formatting

--- src/do_not_optimize.cu
+++ src/do_not_optimize.cu
@@ -1,9 +1,9 @@
 #include "scope/do_not_optimize.hu"
 
-template<> __device__ void do_not_optimize<int32_t>(const int32_t& t) {
-    asm volatile("" ::"r"(t) : "memory");
-  }
-  
-  template<> __device__ void do_not_optimize<int64_t>(const int64_t& t) {
-    asm volatile("" ::"l"(t) : "memory");
-  }
+template <> __device__ void do_not_optimize<int32_t>(const int32_t &t) {
+  asm volatile("" ::"r"(t) : "memory");
+}
+
+template <> __device__ void do_not_optimize<int64_t>(const int64_t &t) {
+  asm volatile("" ::"l"(t) : "memory");
+}
