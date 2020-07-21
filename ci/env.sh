17342fe5b9a2c512235ff694012e5eba419f2d13
Carl Pearson <carl.w.pearson@gmail.com>
2020-07-21 13:12:24 -0600

    [travis] CUDA 11
    commit 69d2bc2b2456fe61c7f54698538df8b41963af84
    Author: Carl Pearson <carl.w.pearson@gmail.com>
    Date:   Tue Jul 21 13:04:15 2020 -0600
    
        cuda 10.2 build
    
    commit c21b5d795ee2039c66266c89ee946c9755d87214
    Author: Carl Pearson <carl.w.pearson@gmail.com>
    Date:   Tue Jul 21 12:58:59 2020 -0600
    
        cuda 11

--- ci/env.sh
+++ ci/env.sh
@@ -1,10 +1,13 @@
 CMAKE_PREFIX=$HOME/cmake
 
-# default unset variables to 1
+# default enable all components
 if [ -z ${USE_CUDA+x} ]; then USE_CUDA=1; fi
 if [ -z ${USE_OPENMP+x} ]; then USE_OPENMP=1; fi
 if [ -z ${USE_NUMA+x} ]; then USE_NUMA=1; fi
 if [ -z ${USE_NVTX+x} ]; then USE_NVTX=1; fi
+
+#default CUDA 11.0
+if [ -z ${CUDA_VERSION+x} ]; then CUDA_VERSION=110; fi
 
 export PATH=/usr/local/cuda/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
