6189d40a4b2221faf62de99ac32fea511f7b12ca
Carl Pearson <cwpears@sandia.gov>
2022-11-07 12:58:50 -0700

    add scope::time_point

--- include/scope/chrono.hpp
+++ include/scope/chrono.hpp
@@ -3,4 +3,5 @@
 namespace scope {
     using clock = std::chrono::high_resolution_clock;
     using duration = std::chrono::duration<double>;
+    using time_point = std::chrono::time_point<clock>;
 }
