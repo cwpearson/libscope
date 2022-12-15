739fbb557f17e199bdcafe844029c6c7c5da121f
Carl Pearson <pearson@illinois.edu>
2022-12-15 18:01:32 -0500

    do_not_optimize for HIP

--- include/scope/do_not_optimize.hpp
+++ include/scope/do_not_optimize.hpp
@@ -7,11 +7,12 @@ void __attribute__((always_inline)) do_not_optimize(Tp const& value) {
   asm volatile("" : : "r,m"(value) : "memory");
 }
 
+// https://gcc.gnu.org/onlinedocs/gcc/Machine-Constraints.html#Machine-Constraints
 template <class Tp>
 __device__
 void __attribute__((always_inline)) do_not_optimize(Tp& value) {
 #if defined(__HIP_DEVICE_COMPILE__)
-  // asm volatile("" : "+m,r"(value) : : "memory");
+  asm volatile("" : "+v"(value) : : "memory");
 #else
   asm volatile("" : "+r,m"(value) : : "memory");
 #endif
