0439d9f9fd67a5dc4246f261a8a2dd456892b97c
Carl Pearson <pearson@illinois.edu>
2022-12-15 16:37:59 -0500

    clobber for AMD GPUs

--- include/scope/do_not_optimize.hpp
+++ include/scope/do_not_optimize.hpp
@@ -10,6 +10,10 @@ void __attribute__((always_inline)) do_not_optimize(Tp const& value) {
 template <class Tp>
 __device__
 void __attribute__((always_inline)) do_not_optimize(Tp& value) {
+#if defined(__HIP_DEVICE_COMPILE__)
+  // asm volatile("" : "+m,r"(value) : : "memory");
+#else
   asm volatile("" : "+r,m"(value) : : "memory");
+#endif
 }
 }
